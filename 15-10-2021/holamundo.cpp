e6817d36a3407c80f01f9958d0fb9bf2afad8d5b
jbelenob <jbelenob@unal.edu.co>
2021-10-15 09:25:23 -0500

    ejemplo variables

---
+++ 15-10-2021/holamundo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "hola mundo\n";
+    int a;
+    float x;
+    double y = 0.0;
+
+    std::cout << "a = " << a << "\n ";
+    std::cout << "x = " << x << "\n";
+    std::cout << "y = " << y << "\n";
+
+    return 0;
+
+    
+}
